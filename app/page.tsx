75a20774e212c235745fdefeae67637d510678cb
Ian <istreator@yahoo.com>
2023-07-31 03:35:00 -0500

    built out page structure, brought in Contentful API

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,44 @@
-import Image from 'next/image'
+const contentful = require("contentful");
+import { ProductStructure, ContentfulProductStructure } from "@/types";
+import Banner from "./components/Banner";
+import ProductCard from "./components/ProductCard";
 
-export default function Home() {
+export default async function Home() {
+  const items = await fetchDataFromApi();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="relative max-h-screen flex flex-col items-center justify-between overflow-hidden">
+      <Banner />
+
+      <div className="flex flex-wrap px-6 pb-6 justify-between overflow-y-scroll bg-accent backdrop">
+        {items.map((data, i) => (
+          <ProductCard key={i} {...data} />
+        ))}
+      </div>
     </main>
-  )
+  );
 }
+
+export const fetchDataFromApi = async () => {
+  const client = contentful.createClient({
+    accessToken: process.env.CDA_TOKEN,
+    space: process.env.SPACE_ID,
+  });
+
+  const content: ContentfulProductStructure = await client.getEntries({
+    content_type: "products",
+  });
+
+  const items: ProductStructure[] = content.items.map(
+    ({ fields: { name, available, description, price, image } }) => {
+      const urls = image.map((img) => img.fields.file.url);
+      return {
+        name,
+        urls,
+        price,
+        description,
+        available,
+      };
+    }
+  );
+  return items;
+};
