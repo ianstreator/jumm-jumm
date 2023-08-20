ff99ed035a9ff653ebced07b35a7e59ca3a5f460
Ian <istreator@yahoo.com>
2023-08-20 13:50:01 -0500

    created categorized products object and mapped over it.

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,26 @@ export default async function Home() {
     <main className="relative max-h-screen flex flex-col items-center justify-between overflow-hidden">
       <Banner {...products} />
 
-      <div className="flex flex-wrap px-6 pb-6 justify-between overflow-y-scroll bg-accent backdrop">
-        {/* {products.map((data, i) => (
-          <ProductCard key={i} {...data} />
-        ))} */}
+      <div className="bg-accent backdrop overflow-scroll relative">
+        {Object.entries(products).map(([category, subcategories], i) => (
+          <div className="relative">
+            <h1 className="sticky top-0 z-30 font-bold text-white bg-secondary text-2xl text-end w-full p-2 px-4">
+              {category}
+            </h1>
+            {Object.entries(subcategories).map(([subcategory, products]) => (
+              <div className="relative">
+                <h3 className="sticky top-0 z-50 font-light text-2xl p-2 px-4 rounded-r-md bg-info w-fit">
+                  {subcategory}
+                </h3>
+                <div className="flex flex-wrap px-6 pb-6 justify-start">
+                  {products.map((product, i) => (
+                    <ProductCard key={i} {...product} />
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
     </main>
   );
@@ -33,23 +49,26 @@ const fetchContentful = async () => {
   });
 
   const items: ProductStructure[] = content.items.map(
-    ({
-      fields: {
-        name,
-        available,
-        description,
-        price,
-        image,
-        subcategory: {
-          fields: {
-            title: subcategoryName,
-            category: {
-              fields: { title: categoryName },
+    (
+      {
+        fields: {
+          name,
+          available,
+          description,
+          price,
+          image,
+          subcategory: {
+            fields: {
+              title: subcategoryName,
+              category: {
+                fields: { title: categoryName },
+              },
             },
           },
         },
       },
-    }) => {
+      i
+    ) => {
       const urls = image.map((img) => img.fields.file.url);
 
       return {
@@ -78,13 +97,11 @@ const fetchContentful = async () => {
     }
 
     if (!products[categoryName!][subcategoryName!]) {
-      products[categoryName!][subcategoryName!] = [];
+      products[categoryName!][subcategoryName!] = [product];
     } else {
       products[categoryName!][subcategoryName!].push(product);
     }
   });
-
-  console.log(products);
 
   return products;
 };
