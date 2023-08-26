65f47d116e3fbc289caab4f494b04008d9d917b8
Ian <istreator@yahoo.com>
2023-08-26 15:19:23 -0500

    renamed component and brought in cart

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,40 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useContext } from "react";
 import { AppContext } from "../context";
 
 function Navbar() {
-  const { menuState, setMenuState } = useContext(AppContext);
+  const { menuState, setMenuState, cartState, setCartState, cartProducts } =
+    useContext(AppContext);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    setCartCount(cartProductsSum());
+  }, [cartProducts]);
+
+  const cartProductsSum = () => {
+    let count = 0;
+
+    Object.values(cartProducts).forEach((category) => {
+      Object.values(category).forEach((subcategory) => {
+        Object.values(subcategory).forEach(
+          (product) => (count += product.count)
+        );
+      });
+    });
+
+    return count;
+  };
 
   return (
     <div className="bg-primary w-full flex items-center justify-between p-2 px-6">
       <button
         className="btn btn-sm bg-secondary text-white border-none rounded-md"
-        onClick={() => setMenuState(!menuState)}
+        onClick={() => {
+          setMenuState(!menuState);
+          setCartState(false);
+        }}
       >
         menú
       </button>
@@ -23,7 +46,27 @@ function Navbar() {
           alt="logo"
         ></Image>
       </div>
-      <Image src={"/basket.svg"} width={50} height={50} alt="basket"></Image>
+      <div
+        onClick={() => {
+          if (!Object.keys(cartProducts).length)
+            return alert("Add items to view cart!");
+          setCartState(!cartState);
+          setMenuState(false);
+        }}
+        className="relative flex flex-row cursor-pointer"
+      >
+        <Image
+          src={"/basket.svg"}
+          width={50}
+          height={50}
+          alt="basket"
+        ></Image>
+        {cartCount > 0 && (
+          <div className="absolute left-6 rounded-full bg-secondary text-white flex flex-col justify-center items-center w-8 h-8 p-3">
+            {cartCount}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
