9332bacf4131c569aa43de1c54cf881d5f65eac4
Ian <istreator@yahoo.com>
2023-08-26 15:18:41 -0500

    added cart functions to product cards, plus slight style changes

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,13 +1,34 @@
 "use client";
 import Image from "next/image";
 import { ProductStructure } from "@/types";
+import { useContext, useEffect } from "react";
+import { AppContext } from "../context";
 
-function ProductCard(data: ProductStructure) {
-  const { urls, name, price } = data;
+function ProductCard(product: ProductStructure) {
+  const { urls, name, price, categoryName, subcategoryName } = product;
+
+  const { addProductToCart } = useContext(AppContext);
+
+  const cartProduct = {
+    name,
+    price,
+    urls,
+    categoryName,
+    subcategoryName,
+  };
 
   return (
-    <div key={name} id={name} className="card card-side rounded-sm w-44 card-min bg-primary shadow-lg my-4 mx-auto overflow-hidden">
-      <Image src={`https:${urls[0]}`} width={175} height={175} alt={name}></Image>
+    <div
+      key={name}
+      id={name}
+      className="card card-side rounded-sm w-44 card-min bg-primary shadow-lg my-4 mx-auto overflow-hidden"
+    >
+      <Image
+        src={`https:${urls[0]}`}
+        width={175}
+        height={175}
+        alt={name}
+      ></Image>
 
       <div className="card-body p-2 justify-between">
         <div>
@@ -15,7 +36,10 @@ function ProductCard(data: ProductStructure) {
           <p>${price.toFixed(2)}</p>
         </div>
 
-        <button className="btn btn-sm bg-secondary border-none text-xs text-white">
+        <button
+          onClick={() => addProductToCart(cartProduct)}
+          className="btn btn-sm bg-secondary border-none text-xs text-white"
+        >
           <Image
             src={"/basket-plus.svg"}
             width={30}
