9798fbac4233261e36e6ca0b24e2495e616f18b7
bob <istreator@yahoo.com>
2023-09-23 15:48:48 -0500

    updated theme colors to meet contrast standards

--- app/components/CategorizedProducts.tsx
+++ app/components/CategorizedProducts.tsx
@@ -3,28 +3,43 @@ import { Products } from "@/types";
 import ProductCard from "./ProductCard";
 
 function CategorizedProducts(products: Products) {
-  console.log(products)
   return (
     <div className="overflow-y-scroll grow relative w-full">
-      {Object.entries(products).map(([category, subcategories], categoryIndex) => (
-        <div key={categoryIndex}>
-          <h1 className="sticky top-0 z-30 font-bold text-white bg-secondary text-2xl text-end w-full p-2 px-4 shadow-md">
-            {category}
-          </h1>
-          {Object.entries(subcategories).map(([subcategory, products], subcategoryIndex) => (
-            <div key={subcategoryIndex} id={products[0].name} className="relative">
-              <h3 className="sticky top-0 z-30 font-med text-2xl p-2 px-4 bg-info w-fit">
-                {subcategory}
-              </h3>
-              <div className="relative w-full flex flex-wrap p-6 justify-center md:justify-start">
-                {products.map((product, productIndex) => (
-                  <ProductCard key={productIndex} product={product} priority={categoryIndex < 1 && subcategoryIndex < 1 && productIndex < 6} />
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
-      ))}
+      {Object.entries(products).map(
+        ([category, subcategories], categoryIndex) => (
+          <div key={categoryIndex}>
+            <h1 className="sticky top-0 z-30 font-bold text-white bg-secondary text-2xl text-end w-full p-2 px-4 shadow-md">
+              {category}
+            </h1>
+            {Object.entries(subcategories).map(
+              ([subcategory, products], subcategoryIndex) => (
+                <div
+                  key={subcategoryIndex}
+                  id={products[0].name}
+                  className="relative"
+                >
+                  <h3 className="sticky top-0 z-30 font-med text-2xl p-2 px-4 bg-info w-fit">
+                    {subcategory}
+                  </h3>
+                  <div className="relative w-full flex flex-wrap p-6 justify-center md:justify-start">
+                    {products.map((product, productIndex) => (
+                      <ProductCard
+                        key={productIndex}
+                        product={product}
+                        priority={
+                          categoryIndex < 1 &&
+                          subcategoryIndex < 1 &&
+                          productIndex < 6
+                        }
+                      />
+                    ))}
+                  </div>
+                </div>
+              )
+            )}
+          </div>
+        )
+      )}
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import { cache } from "react"
-import {
-  ProductStructure,
-  Products,
-} from "@/types";
+import { cache } from "react";
+import { ProductStructure, Products } from "@/types";
 import "react-toastify/dist/ReactToastify.css";
 
 import CategorizedProducts from "./components/CategorizedProducts";
@@ -11,11 +8,10 @@ import Menu from "./components/Menu";
 import Cart from "./components/Cart";
 import { ToastContainer } from "react-toastify";
 
-export const revalidate = 3600
+export const revalidate = 3600;
 
 export default async function Home() {
   const { products } = await fetchContentful();
-
 
   return (
     <main className="relative h-full flex flex-col items-center justify-between backdrop bg-accent md:px-40">
@@ -33,73 +29,77 @@ export default async function Home() {
     </main>
   );
 }
-export const dynamic = 'force-static'
+export const dynamic = "force-static";
 
 const fetchContentful = cache(async () => {
+  const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+  const SPACE_ID = process.env.SPACE_ID;
+  const ENVIRONMENT_ID = process.env.ENVIRONMENT_ID;
 
-  const ACCESS_TOKEN = process.env.ACCESS_TOKEN
-  const SPACE_ID = process.env.SPACE_ID
-  const ENVIRONMENT_ID = process.env.ENVIRONMENT_ID
+  const BASE_URL = "https://cdn.contentful.com";
 
-  const BASE_URL = "https://cdn.contentful.com"
+  const res = await fetch(
+    `${BASE_URL}/spaces/${SPACE_ID}/entries?content_type=products&include=2&limit=200&access_token=${ACCESS_TOKEN}`
+  );
 
-  const res = await fetch(`${BASE_URL}/spaces/${SPACE_ID}/environments/${ENVIRONMENT_ID}/entries?content_type=products&include=2&limit=200&access_token=${ACCESS_TOKEN}`)
+  const products: Products = await res.json().then((data) => {
+    const {
+      includes: { Entry, Asset },
+      items,
+    } = data;
 
-  const products: Products = await res.json().then(
-    data => {
-      const { includes: { Entry, Asset }, items } = data
+    const imagesMap: any = Object.values(Asset).reduce((map: any, ass: any) => {
+      map[ass.sys.id] = ass.fields.file.url;
+      return map;
+    }, {});
 
-      const imagesMap: any = Object.values(Asset).reduce((map: any, ass: any) => {
-        map[ass.sys.id] = ass.fields.file.url;
-        return map;
-      }, {});
+    const categories: any = {};
+    const subcategories: any = {};
 
-      const categories: any = {}
-      const subcategories: any = {}
+    for (const entry of Entry) {
+      const isCategory = !entry.fields.category;
+      const id = entry.sys.id;
+      const title = entry.fields.title;
 
-      for (const entry of Entry) {
-        const isCategory = !entry.fields.category
-        const id = entry.sys.id
-        const title = entry.fields.title
-
-        if (isCategory) {
-          categories[id] = title
-        } else {
-          const categoryPointer = entry.fields.category.sys.id
-          subcategories[id] = { subcategoryName: title, categoryPointer }
-        }
+      if (isCategory) {
+        categories[id] = title;
+      } else {
+        const categoryPointer = entry.fields.category.sys.id;
+        subcategories[id] = { subcategoryName: title, categoryPointer };
       }
+    }
 
-      const products = Object.values(items).reduce((products: Products, item: any) => {
-        const imageID = item.fields.image[0].sys.id
-        const subcategoryID = item.fields.subcategory.sys.id
-        const { subcategoryName, categoryPointer } = subcategories[subcategoryID]
-        const categoryName = categories[categoryPointer]
-        const url = imagesMap[imageID]
+    const products = Object.values(items).reduce(
+      (products: Products, item: any) => {
+        const imageID = item.fields.image[0].sys.id;
+        const subcategoryID = item.fields.subcategory.sys.id;
+        const { subcategoryName, categoryPointer } =
+          subcategories[subcategoryID];
+        const categoryName = categories[categoryPointer];
+        const url = imagesMap[imageID];
 
-        delete item.fields.image
-        delete item.fields.subcategory
+        delete item.fields.image;
+        delete item.fields.subcategory;
 
         const product: ProductStructure = {
           ...item.fields,
           url,
           subcategoryName,
-          categoryName
-        }
+          categoryName,
+        };
 
         products[categoryName] = products[categoryName] || {};
-        products[categoryName][subcategoryName] = products[categoryName][subcategoryName] || [];
+        products[categoryName][subcategoryName] =
+          products[categoryName][subcategoryName] || [];
         products[categoryName][subcategoryName].push(product);
 
-        return products
-      }, {})
+        return products;
+      },
+      {}
+    );
 
-      return products
-    }
-  )
-
+    return products;
+  });
 
   return { products };
-
 });
-
